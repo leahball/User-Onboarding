0a30c7920951c629018ddc3fb55dd42be449c02f
iamleahb <leah.ball@gmail.com>
2021-12-17 02:25:33 -0600

    cypress tests working mvp met

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Form from "./Components/Form";
-import FormResults from "./Components/FormResults";
+// import FormResults from "./Components/FormResults";
 import axios from "axios";
 import schema from "../src/Validation/formSchema";
 import * as yup from "yup";
@@ -18,32 +18,55 @@ const initialFormErrors = {
   password: "",
 };
 
-const initialUser = [];
-// const initialDisabled = true;
+const initialUsers = [];
+const initialDisabled = true;
 
 function App() {
   const [users, setUsers] = useState([]);
   const [formValues, setFormValues] = useState(initialFormValues);
   const [formErrors, setFormErrors] = useState(initialFormErrors);
-  // const [disabled, setDisabled] = useState(initialDisabled);
+  const [disabled, setDisabled] = useState(initialDisabled);
 
-  //Helpers
-  // const getUsers = () => {
-  //   //get users
-  // };
-  // const postUsers = () => {
-  //   //post users to state
-  // };
-
-  const handleSubmit = () => {
+  const getUsers = () => {
     axios
-      .post("http://reqres.in/api/users", formValues)
+      .get("https://reqres.in/api/users")
+      .then((res) => {
+        setUsers(res.data.data);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const postNewUser = (newUser) => {
+    axios
+      .post("https://reqres.in/api/users", newUser)
       .then((res) => {
         setUsers([res.data, ...users]);
       })
       .catch((err) => console.error(err))
-      .finally(() => setFormValues(initialFormValues));
+      .finally(() => {
+        setFormValues(initialFormValues);
+      });
   };
+
+  const handleSubmit = () => {
+    const newUser = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      tos: formValues.tos,
+    };
+    postNewUser(newUser);
+  };
+
+  //   axios
+  //     .post("http://reqres.in/api/users", formValues)
+  //     .then((res) => {
+  //       setUsers([res.data, ...users]);
+  //     })
+  //     .catch((err) => console.error(err))
+  //     .finally(() => setFormValues(initialFormValues));
+  // };
+
   const handleChange = (name, value) => {
     validate(name, value);
     setFormValues({ ...formValues, [name]: value });
@@ -57,6 +80,14 @@ function App() {
       .catch((err) => setFormErrors({ ...formErrors, [name]: err.errors[0] }));
   };
 
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  useEffect(() => {
+    schema.isValid(formValues).then((valid) => setDisabled(!valid));
+  }, [formValues]);
+
   return (
     <div className="App">
       <Form
@@ -64,6 +95,7 @@ function App() {
         change={handleChange}
         errors={formErrors}
         submit={handleSubmit}
+        disabled={disabled}
       />
       {users.map((user) => (
         <div key={user.id}>
